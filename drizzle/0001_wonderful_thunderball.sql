a54bf30582f3805bb10b93f143768eb26de45d02
Tim Almamedov <tim.almamedov@gmail.com>
2026-07-16 09:16:58 +0200

    Add follow-up migration for material fields

---
+++ drizzle/0001_wonderful_thunderball.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "weld_joints" ADD COLUMN "material_unique_number_1" text;--> statement-breakpoint
+ALTER TABLE "weld_joints" ADD COLUMN "material_unique_number_2" text;--> statement-breakpoint
+ALTER TABLE "weld_joints" ADD COLUMN "material_full_name_1" text;--> statement-breakpoint
+ALTER TABLE "weld_joints" ADD COLUMN "material_full_name_2" text;--> statement-breakpoint
+ALTER TABLE "weld_joints" ADD COLUMN "material_normative_document_1" text;--> statement-breakpoint
+ALTER TABLE "weld_joints" ADD COLUMN "material_normative_document_2" text;--> statement-breakpoint
+ALTER TABLE "weld_joints" ADD COLUMN "material_certificate_number_1" text;--> statement-breakpoint
+ALTER TABLE "weld_joints" ADD COLUMN "material_certificate_number_2" text;
